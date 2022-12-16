6f0d1160ba059a2e6ab7f1da29bc050da55b3289
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-12-16 11:45:38 +0800

    initialize xylophone app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const XyloPhoneApp());
+}
+
+class XyloPhoneApp extends StatelessWidget {
+  const XyloPhoneApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
