d92ba2c5e3b9a3b96be274c093a9f772141bff50
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-12-17 13:35:02 +0800

    added functionality for playing different music in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  runApp(const XyloPhoneApp());
+  runApp(XyloPhoneApp());
 }
 
 class XyloPhoneApp extends StatelessWidget {
-  const XyloPhoneApp({Key? key}) : super(key: key);
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                onPressed: () {
+                  playSound(1);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.orange,
+                ),
+                onPressed: () {
+                  playSound(2);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.yellow,
+                ),
+                onPressed: () {
+                  playSound(3);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green,
+                ),
+                onPressed: () {
+                  playSound(4);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                ),
+                onPressed: () {
+                  playSound(5);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                onPressed: () {
+                  playSound(6);
+                },
+                child: Text(''),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                ),
+                onPressed: () {
+                  playSound(7);
+                },
+                child: Text(''),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
